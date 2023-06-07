8046649f0c6da1e9ff4e4c93366a1afd26ccfbc1
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-06-07 10:07:18 +0300

    done: Profile, Statistics

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,19 @@
+import user from '../data/user.json';
+import data from '../data/data.json';
+
+import Profile from 'components/profile';
+import Statistics from 'components/statistics';
+
 const App = () => {
-    return <div className="box">hello world
-        <p>123</p>
-        <span>456</span>
-        <a href="/">go to</a>
+    return <div className="app-container">
+        <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats} />
+        <Statistics title="Upload stats" stats={data} />
+        {/* <Statistics stats={data} /> якщо розкомітити, то двоїть рендер */}
     </div>
 }
 
