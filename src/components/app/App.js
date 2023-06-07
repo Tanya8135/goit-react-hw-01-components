bbad2f4709058345a5e687d951bbd682350787be
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-06-07 10:47:55 +0300

    done Friend, Transaction

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,12 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 import Profile from 'components/profile';
 import Statistics from 'components/statistics';
+import Friend from 'components/friend';
+import Transaction from 'components/transaction';
 
 const App = () => {
     return <div className="app-container">
@@ -14,6 +18,8 @@ const App = () => {
             stats={user.stats} />
         <Statistics title="Upload stats" stats={data} />
         {/* <Statistics stats={data} /> якщо розкомітити, то двоїть рендер */}
+        <Friend friends={friends} />
+        <Transaction items={transactions} />
     </div>
 }
 
---
+++ src/components/transaction/Transaction.js
@@ -0,0 +1,13 @@
+import TransactionHistory from "./transactionHistory";
+
+const Transaction = () => {
+    return (
+        <div>
+            <table className="transaction-history">
+                <TransactionHistory />
+            </table>
+        </div>
+    )
+}
+
+export default Transaction;
