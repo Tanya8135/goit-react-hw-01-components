3077a1a90bb1de98071edeef132ec5ebbcd4acdb
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-06-17 21:41:54 +0300

    change

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+    return <div className="App">
+        home work
+    </div>
+}
+
+export default App;
+
+/* Порядок сортування import */
+// - спочатку import бібліотек;
+// - import власних компонентів;
+// - import шнших файлів, ті, що не є компонентами: файли json, css, scss і тому подібне
